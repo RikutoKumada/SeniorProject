7d30ac97c8d677343c4594ea4afd138daeb4ab04
熊田 陸人 <k20059rk@apps.kct.ac.jp>
2024-11-07 16:57:51 +0900

    Delete MainProgram directory

--- MainProgram/MainRobot/QLearning.c
+++
@@ -1,7 +0,0 @@
-#include "QLearning.h"
-#include <stdint.h>
-
-uint8_t LearnAndReturnAction(uint8_t *dists, uint8_t distsSize, ControllerInput *inputs, uint8_t inputsSize) {
-    // ここにQ学習の処理を書く
-    return inputs[0].angle;
-}
